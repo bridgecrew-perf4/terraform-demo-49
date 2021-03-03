9700480e102fec5e18b6424efbc734ea30a5fe74
eager <eager.yu@splashtop.com>
2021-03-03 11:09:28 +0800

    update backend settings

--- backend.tf
+++ backend.tf
@@ -14,7 +14,7 @@ terraform {
     shared_credentials_file = "auth"
     profile                 = "s3_dynamodb"
     bucket         = "terraform-up-and-running-state-eager-1"
-    key            = "scg/${var.region}/scg-${var.region}-.tfstate"
+    key            = "[[s3_path]]"
     region         = "us-east-2"
     dynamodb_table = "terraform-up-and-running-locks_eager"
     encrypt        = true
